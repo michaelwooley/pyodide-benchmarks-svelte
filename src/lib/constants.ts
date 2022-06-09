a0a2150559ae6b2ba354d9d70af10c723be924fb
Michael Wooley <wm.wooley@gmail.com>
2022-06-08 20:26:26 -0400

    Add on basic project setup

---
+++ src/lib/constants.ts
@@ -0,0 +1,11 @@
+/**
+ * Reference constants
+ */
+
+export const PROJECT_NAME = 'pybros';
+export const PROJECT_EMOJI = '🥧';
+export const PROJECT_REPO = 'https://github.com/michaelwooley/pybros';
+
+export const PYODIDE_INDEX_URL = `https://cdn.jsdelivr.net/pyodide/v${
+    import.meta.env.VITE_PYODIDE_VERSION ||  "v0.20.1a1"
+}/full/`;
