781b67b6b82ba96a4ec94d1d05f6bcb649759668
Michael Wooley <wm.wooley@gmail.com>
2022-06-08 23:26:33 -0400

    fix style

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,10 +2,10 @@
  * Reference constants
  */
 
-export const PROJECT_NAME = 'pybros';
+export const PROJECT_NAME = 'pyodide benchmarks';
 export const PROJECT_EMOJI = '🥧';
-export const PROJECT_REPO = 'https://github.com/michaelwooley/pybros';
+export const PROJECT_REPO = 'https://github.com/michaelwooley/pyodide-benchmarks-svelte';
 
 export const PYODIDE_INDEX_URL = `https://cdn.jsdelivr.net/pyodide/v${
-    import.meta.env.VITE_PYODIDE_VERSION ||  "v0.20.1a1"
+    import.meta.env.VITE_PYODIDE_VERSION || 'v0.20.1a1'
 }/full/`;
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import '../app.scss';
-	import { PROJECT_NAME } from '$lib/constants';
+    import '../app.scss';
+    import { PROJECT_NAME } from '$lib/constants';
 </script>
 
 <svelte:head>
-	<title>{PROJECT_NAME}</title>
+    <title>{PROJECT_NAME}</title>
 </svelte:head>
 
 <main id="svelte">
-	<slot />
+    <slot />
 </main>
