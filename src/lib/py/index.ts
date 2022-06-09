b05ec56b892fa602d1b1ac5d39d07a62bb0524e9
Michael Wooley <wm.wooley@gmail.com>
2022-06-08 23:26:27 -0400

    Modify pyodide caller

--- src/lib/py/index.ts
+++ src/lib/py/index.ts
@@ -3,21 +3,36 @@
  */
 
 import { PYODIDE_INDEX_URL } from '$lib/constants';
-import * as pyodide_pkg from 'pyodide';
+import * as pyodidePkg from 'pyodide';
 
 export async function loadPyodide() {
-    // const pyodide_pkg = await import('pyodide');
-    const pyodide = await pyodide_pkg.loadPyodide({
-        indexURL: PYODIDE_INDEX_URL
+    const pyodide = await pyodidePkg.loadPyodide({
+        indexURL: PYODIDE_INDEX_URL,
+        stdout: console.info,
+        stderr: console.warn
     });
 
-    pyodide.runPython(`
-        import js
-        main_div = js.document.getElementById("status")
-        if main_div:
-          main_div.innerHTML = "Loaded!"
-        div = js.document.createElement("div")
-        div.innerHTML = "<h1>Hello Pyodide! This element was created from Python</h1>"
-        js.document.body.prepend(div)
+    const out = pyodide.runPython(`
+    import sys
+    from pyodide.console import PyodideConsole, repr_shorten, BANNER
+    import __main__
+    pyconsole = PyodideConsole(__main__.__dict__)
+    {"banner": BANNER}
     `);
+
+    // const out = pyodide.runPython(`
+    //     import js
+    //     main_div = js.document.getElementById("status")
+    //     if main_div:
+    //       main_div.innerHTML = "Loaded!"
+    //     div = js.document.createElement("div")
+    //     div.innerHTML = "<h1>Hello Pyodide! This element was created from Python</h1>"
+    //     js.document.body.prepend(div)
+
+    //     print("from stdout!")
+
+    //     "here"
+    // `);
+
+    console.log('output: ', out.toJs());
 }
