8987eb3ea79cbe45e517aca65d2c5f7056b79841
Michael Wooley <wm.wooley@gmail.com>
2022-06-08 23:56:22 -0400

    Add some timging

--- src/containers/PyodideMainThreadDemo.svelte
+++ src/containers/PyodideMainThreadDemo.svelte
@@ -1,19 +1,43 @@
 <script lang="ts">
     import { onMount } from 'svelte';
 
+    let out: string, meas: PerformanceEntry;
+
     onMount(async () => {
         console.log('Loading pyodide');
         const loadPyodide = await (await import('$lib/py')).loadPyodide;
         console.log(loadPyodide);
-        await loadPyodide();
-        // const mod = await import('$lib/rustPython/pkg/rustpython_wasm');
-        // await mod.default();
-        // pyExec = mod.pyExec;
-        // pyExec('1+1'); // Warm up
+        const markStart = 'markStart',
+            markEnd = 'markEnd';
+        performance.mark(markStart);
+        out = await loadPyodide().then((b) => {
+            performance.mark(markEnd);
+            return b;
+        });
+        performance.measure('loadPyodide', markStart, markEnd);
+        meas = performance.getEntriesByName('loadPyodide', 'measure')[0];
+
+        console.log(out, meas);
     });
 </script>
 
+<h1 class="title is-1">
+    Pyodide on main thread (See issue <a
+        href="https://github.com/michaelwooley/pyodide-benchmarks-svelte/issues/4"
+        target="_blank">#4</a
+    >)
+</h1>
 <div id="status">Status stuff...</div>
+{#if out}
+    <div>
+        <pre>{out}</pre>
+    </div>
+{/if}
+{#if meas}
+    <ul>
+        <li>{meas.name} : {(meas.duration / 1000).toPrecision(3)}s.</li>
+    </ul>
+{/if}
 
 <style lang="scss">
 </style>
--- src/lib/py/index.ts
+++ src/lib/py/index.ts
@@ -6,33 +6,25 @@ import { PYODIDE_INDEX_URL } from '$lib/constants';
 import * as pyodidePkg from 'pyodide';
 
 export async function loadPyodide() {
+    // const pyodidePkg = await import('pyodide'); // Really need here?
     const pyodide = await pyodidePkg.loadPyodide({
         indexURL: PYODIDE_INDEX_URL,
         stdout: console.info,
         stderr: console.warn
     });
 
-    const out = pyodide.runPython(`
+    const banner: string = pyodide.runPython(
+        `
     import sys
-    from pyodide.console import PyodideConsole, repr_shorten, BANNER
+    from pyodide.console import PyodideConsole, BANNER
     import __main__
     pyconsole = PyodideConsole(__main__.__dict__)
-    {"banner": BANNER}
-    `);
 
-    // const out = pyodide.runPython(`
-    //     import js
-    //     main_div = js.document.getElementById("status")
-    //     if main_div:
-    //       main_div.innerHTML = "Loaded!"
-    //     div = js.document.createElement("div")
-    //     div.innerHTML = "<h1>Hello Pyodide! This element was created from Python</h1>"
-    //     js.document.body.prepend(div)
+    # return value
+    # sys.version
+    BANNER #{"banner": BANNER}
+    `
+    );
 
-    //     print("from stdout!")
-
-    //     "here"
-    // `);
-
-    console.log('output: ', out.toJs());
+    return banner;
 }
