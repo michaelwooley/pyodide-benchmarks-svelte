745debb0437589a5bee38b0117ee6a14c3a32c67
Michael Wooley <wm.wooley@gmail.com>
2022-07-19 02:03:45 -0400

    set up demo components

---
+++ src/components/SimpleRepl/SimpleRepl.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import type { TCodeExample } from '$lib/py/examples';
+    import InputPanel from './InputPanel.svelte';
+    import OutputPanel from './OutputPanel.svelte';
+
+    export let command: string;
+    export let output: string[] = [];
+    export let codeExamples: TCodeExample[] = [];
+    export let isRunning: boolean;
+    export let isLoaded: boolean;
+</script>
+
+<div class="content">
+    <h1 class="title is-1">
+        <slot name="title">
+            <span> Simple repl </span>
+        </slot>
+    </h1>
+
+    <!-- <ul>
+        <li><strong>Status </strong>: {py ? '✅ Ready.' : '⏳ Loading...'}</li>
+        <li>⚠️ The browser will probably feel like it is "freezing" until the load is complete.</li>
+    </ul> -->
+    <slot name="info">
+        <span />
+    </slot>
+</div>
+
+<slot name="input">
+    <InputPanel {command} {codeExamples} {isRunning} {isLoaded} on:submit />
+</slot>
+
+<hr />
+<slot name="output">
+    <OutputPanel {output} />
+</slot>
---
+++ src/components/SimpleRepl/index.ts
@@ -0,0 +1,1 @@
+export { default as SimpleRepl } from './SimpleRepl.svelte';
